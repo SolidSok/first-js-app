59e6f48e6de18526a5a9ea35c59b89e53df74996
SolidSok <solidsok@gmail.com>
2022-04-28 21:09:17 -0700

    deleted demo, added pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
-alert('Hello world');
-let favoriteFood = 'meat';
-document.write(favoriteFood)
+let pokemonlist = [
+    {
+        name: 'Charmander',
+        height: 2,
+        type: 'fire',
+        weaknesses: ['water', 'ground', 'rock']
+    },
+    {
+        name: 'Butterfree',
+        height: 3,
+        type: ['bug', 'flying'],
+        weaknesses: ['fire', 'flying', 'electric', 'ice', 'rock']
+    },
+    {
+        name: 'Vulpix',
+        height: 2,
+        type: 'fire',
+        weaknesses: ['water', 'ground', 'rock']
+    },
+    {
+        name: 'Meowth',
+        height: 1, 
+        type: 'normal',
+        weaknesses: 'fighting'
+    },
+    {
+        name: 'Ditto',
+        height: 1,
+        type: 'normal',
+        weaknesses: 'fighting'
+    },
+    {
+        name: 'Jolteon',
+        height: 2,
+        type: 'electric',
+        weaknesses: 'ground'
+    }
+];

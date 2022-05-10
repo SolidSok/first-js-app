20be3d56dacf17cc447872fbcbf9565fbece9940
SolidSok <solidsok@gmail.com>
2022-05-09 22:50:54 -0700

    Updated forEach, and created IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,56 @@
-let pokemonList = [
-        {
-            name: 'Charmander',
-            height: 2,
-            type: 'fire',
-            weaknesses: ['water', 'ground', 'rock']
-        },
-        {
-            name: 'Butterfree',
-            height: 3,
-            type: ['bug', 'flying'],
-            weaknesses: ['fire', 'flying', 'electric', 'ice', 'rock']
-        },
-        {
-            name: 'Vulpix',
-            height: 2,
-            type: 'fire',
-            weaknesses: ['water', 'ground', 'rock']
-        },
-        {
-            name: 'Meowth',
-            height: 1, 
-            type: 'normal',
-            weaknesses: 'fighting'
-        },
-        {
-            name: 'Ditto',
-            height: 1,
-            type: 'normal',
-            weaknesses: 'fighting'
-        },
-        {
-            name: 'Jolteon',
-            height: 2,
-            type: 'electric',
-            weaknesses: 'ground'
-        }
-    ]
+let pokemonRepository = (function () {
+    let pokemonList = [
+            {
+                name: 'Charmander',
+                height: 2,
+                type: 'fire',
+                weaknesses: ['water', 'ground', 'rock']
+            },
+            {
+                name: 'Butterfree',
+                height: 3,
+                type: ['bug', 'flying'],
+                weaknesses: ['fire', 'flying', 'electric', 'ice', 'rock']
+            },
+            {
+                name: 'Vulpix',
+                height: 2,
+                type: 'fire',
+                weaknesses: ['water', 'ground', 'rock']
+            },
+            {
+                name: 'Meowth',
+                height: 1, 
+                type: 'normal',
+                weaknesses: 'fighting'
+            },
+            {
+                name: 'Ditto',
+                height: 1,
+                type: 'normal',
+                weaknesses: 'fighting'
+            },
+            {
+                name: 'Jolteon',
+                height: 2,
+                type: 'electric',
+                weaknesses: 'ground'
+            }
+        ];
+   function add(pokemon) {
+       pokemonList.push(pokemon);
+   }
 
-pokemonList.forEach(function (pokemon) {
-    document.write('<p>' + pokemon.name, '(height: ' + pokemon.height + ')</p>')
+   function getALL() {
+       return pokemonList;
+   }
+
+   return {
+       add: add,
+       getALL: getALL
+   };
+})();
+
+pokemonRepository.getALL().forEach(function (pokemon) {
+    document.write('<p>' + pokemon.name + '(height: ' + pokemon.height + ')</p>')
   })()
